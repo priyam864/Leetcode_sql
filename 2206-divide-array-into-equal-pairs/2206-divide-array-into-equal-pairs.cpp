3aaa85bdc7b38a371aec803132c2f84d381d7b63
Priyam Kumari <125537005+priyam864@users.noreply.github.com>
2025-03-17 08:12:30 +0530

    Time: 3 ms (58.51%), Space: 18.1 MB (49.04%) - LeetHub

---
+++ 2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool divideArray(vector<int>& nums) {
+        unordered_map<int, int> freq;
+        for (int num : nums) {
+            freq[num]++;
+        }
+
+        for (auto& [key, count] : freq) {
+            if (count % 2 != 0)
+                return false;
+        }
+
+        return true;
+    }
+};
